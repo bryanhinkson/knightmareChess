0739a12fb110a2cf7526cf3194483aee8daeb76a
Bryan Hinkson <bryan.hinkson@uk2group.com>
2016-09-23 10:00:56 -0600

    test

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -3,4 +3,11 @@ var app = angular.module("knightmareChess", []);
 app.controller('mainController', function($scope){
     $scope.ranks = [1,2,3,4,5,6,7,8];
     $scope.files = ['A','B','C','D','E','F','G','H'];
+
+
+    $scope.startGame = function(){
+        $scope.test = "it works";    
+    }
+
+
 });
--- www/js/index.js
+++ www/js/index.js
@@ -4,8 +4,9 @@ app.controller('mainController', function($scope){
     $scope.ranks = [1,2,3,4,5,6,7,8];
     $scope.files = ['A','B','C','D','E','F','G','H'];
 
+
     $scope.startGame = function(){
-        
+        $scope.test = "it works";    
     }
 
 
