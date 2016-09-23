8f84007894621954400453fcc87019ed7eb4cdb0
Bryan Hinkson <bryan.hinkson@uk2group.com>
2016-09-23 16:53:43 -0600

    Picture pieces and ability to move

--- www/js/index.js
+++ www/js/index.js
@@ -10,65 +10,114 @@ app.controller('mainController', function($scope){
         // Set up white pawns
         var pawns = [document.getElementById('A2'),document.getElementById('B2'),document.getElementById('C2'),document.getElementById('D2'),document.getElementById('E2'),document.getElementById('F2'),document.getElementById('G2'),document.getElementById('H2')];
         for(var i=0; i < pawns.length; i++){
-            pawns[i].innerText= "Pawn";
+            pawns[i].innerHTML= '<img class="chessPiece" src="img/whitePawn.svg">';
         }            
 
         // Set up white Rooks
         var rooks = [document.getElementById('A1'),document.getElementById('H1')];
         for(var i=0; i < rooks.length; i++){
-            rooks[i].innerText= "Rook";
+            rooks[i].innerHTML= '<img class="chessPiece" src="img/whiteRook.svg">';
         }
 
         // Set up white Knights
         var Knights = [document.getElementById('B1'),document.getElementById('G1')];
         for(var i=0; i < Knights.length; i++){
-            Knights[i].innerText= "Knight";
+            Knights[i].innerHTML= '<img class="chessPiece" src="img/whiteKnight.svg">';
         }
 
         // Set up white Bishops
         var Bishops = [document.getElementById('C1'),document.getElementById('F1')];
         for(var i=0; i < Bishops.length; i++){
-            Bishops[i].innerText= "Bishop";
+            Bishops[i].innerHTML= '<img class="chessPiece" src="img/whiteBishop.svg">';
         }
 
         // Set up white King
-        document.getElementById('E1').innerText = "King";
+        document.getElementById('E1').innerHTML= '<img class="chessPiece" src="img/whiteKing.svg">';
 
         // Set up white Queen
-        document.getElementById('D1').innerText = "Queen";
+        document.getElementById('D1').innerHTML= '<img class="chessPiece" src="img/whiteQueen.svg">';
 
         // Set up Black
         // Set up Black pawns
         var pawns = [document.getElementById('A7'),document.getElementById('B7'),document.getElementById('C7'),document.getElementById('D7'),document.getElementById('E7'),document.getElementById('F7'),document.getElementById('G7'),document.getElementById('H7')];
         for(var i=0; i < pawns.length; i++){
-            pawns[i].innerText= "Pawn";
+            pawns[i].innerHTML= '<img class="chessPiece" src="img/blackPawn.svg">';
         }            
 
         // Set up Black Rooks
         var rooks = [document.getElementById('A8'), document.getElementById('H8')];
         for(var i=0; i < rooks.length; i++){
-            rooks[i].innerText= "Rook";
+            rooks[i].innerHTML= '<img class="chessPiece" src="img/blackRook.svg">';
         }
 
         // Set up Black Knights
         var Knights = [document.getElementById('B8'), document.getElementById('G8')];
         for(var i=0; i < Knights.length; i++){
-            Knights[i].innerText= "Knight";
+            Knights[i].innerHTML= '<img class="chessPiece" src="img/blackKnight.svg">';
         }
 
         // Set up Black Bishops
         var Bishops = [document.getElementById('C8'), document.getElementById('F8')];
         for(var i=0; i < Bishops.length; i++){
-            Bishops[i].innerText= "Bishop";
+            Bishops[i].innerHTML= '<img class="chessPiece" src="img/blackBishop.svg">';
         }
 
         // Set up Black King
-        document.getElementById('E8').innerText = "King";
+        document.getElementById('E8').innerHTML= '<img class="chessPiece" src="img/blackKing.svg">';
 
         // Set up Black Queen
-        document.getElementById('D8').innerText = "Queen";
+        document.getElementById('D8').innerHTML= '<img class="chessPiece" src="img/blackQueen.svg">';
 
     }
 
+    $scope.select = function(e){
+
+        console.log(e);
+
+        if($scope.selectedPiece != null){
+            if(e.target.children.length == 0){
+                $scope.newLocation = e.target;
+            }
+            else{
+                $scope.newLocation = e.target;
+            }
+            $scope.move($scope.newLocation);
+            return;
+        }
+
+        if(e.target.children.length == 0){
+            if(e.target.tagName == "IMG"){
+                $scope.selectedPiece = e.target;
+            }
+            else{
+                $scope.selectedPiece = null;
+            }
+        }
+        else{
+            if(e.target.firstChild.tagName == "IMG"){
+                $scope.selectedPiece = e.target.firstChild;
+            }
+            else {
+                $scope.selectedPiece = null;
+            }
+        }
+        console.log($scope.selectedPiece);
+
+        
+    }
+
+    $scope.move = function(newLocation){
+        console.log("Selected Piece:");
+        console.log($scope.selectedPiece);
+        console.log("New Location:");
+        console.log($scope.newLocation);
+
+        $scope.selectedPiece.parentNode.removeChild($scope.selectedPiece);
+        $scope.newLocation.appendChild($scope.selectedPiece)
+
+        $scope.selectedPiece = null;
+        $scope.newLocation = null;
+        return;
+    }
 
 });
